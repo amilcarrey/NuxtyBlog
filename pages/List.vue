9c46fad26e3a441d96f213d4428427a8fb7f89db
Amilcar Rey <amilcarrey.ar@gmail.com>
2020-04-19 19:00:45 -0300

    Layout the site and components

---
+++ pages/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex mb-4">
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from '../components/Post'
+export default {
+  components: {
+    Post
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,20 +9,9 @@
         A little project simulating a blog
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <nuxt-link to="/List" class="button--green">
+          Go to Posts
+        </nuxt-link>
       </div>
     </div>
   </div>
